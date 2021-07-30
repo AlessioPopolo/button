5f3a5d358a345126a1145cde8c10498c233a5612
AlessioPopolo <alessio.popolo@si2001.it>
2021-07-30 14:34:56 +0200

    Modificati dati Mock per test ordinamento su 'cognome'

--- src/app/model/MockDataTable.ts
+++ src/app/model/MockDataTable.ts
@@ -6,13 +6,13 @@ export interface MockDataTable {
 
 export const DATIMOCK: Array<MockDataTable> = [
   { id: 11, nome: 'Dr Nice', cognome: 'Bello' },
-  { id: 12, nome: 'Narco', cognome: 'cognome' },
-  { id: 13, nome: 'Bombasto', cognome: 'cognome' },
-  { id: 14, nome: 'Celeritas', cognome: 'cognome' },
-  { id: 15, nome: 'Magneta', cognome: 'cognome' },
-  { id: 16, nome: 'RubberMan', cognome: 'cognome' },
-  { id: 17, nome: 'Dynama', cognome: 'cognome' },
-  { id: 18, nome: 'Dr IQ', cognome: 'cognome' },
-  { id: 19, nome: 'Magma', cognome: 'cognome' },
-  { id: 20, nome: 'Tornado', cognome: 'cognome' }
+  { id: 12, nome: 'Narco', cognome: 'Traffic' },
+  { id: 13, nome: 'Bombasto', cognome: 'Bombino' },
+  { id: 14, nome: 'Celeritas', cognome: 'Piazza' },
+  { id: 15, nome: 'Magneta', cognome: 'Calamita' },
+  { id: 16, nome: 'RubberMan', cognome: 'Cappello di paglia' },
+  { id: 17, nome: 'Dynama', cognome: 'Ruota' },
+  { id: 18, nome: 'Dr IQ', cognome: '120' },
+  { id: 19, nome: 'Magma', cognome: 'Caldo' },
+  { id: 20, nome: 'Tornado', cognome: 'Mucca' }
 ];
